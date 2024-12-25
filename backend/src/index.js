ae9c78cd873e0d04143dbbd56e5919151f7abaa9
bharaths1803 <bharaths14051803@gmail.com>
2024-12-25 17:31:58 +0530

    Completed profile image upload functionality and extended limits for express.json middleware in index.js file backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -13,7 +14,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+  })
+);
 app.use(cookieParser());
 app.use(
   cors({
