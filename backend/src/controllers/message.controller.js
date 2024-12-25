f500245a10a05571a02cb43b3bd6101039a923b7
bharaths1803 <bharaths14051803@gmail.com>
2024-12-25 13:49:13 +0530

    Completed send message controller and whole backend

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -28,6 +29,30 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log(`Error in get messages controller ${error.message}`);
+    res.status(500).json({ message: `Internal server error` });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const senderId = req.user._id;
+    const { receiverId } = req.params;
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+    res.status(200).json(newMessage);
+  } catch (error) {
+    console.log(`Error in send message controller ${error.message}`);
     res.status(500).json({ message: `Internal server error` });
   }
 };
