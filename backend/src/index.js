56cda1ab48ca45cd746c0ac9ce099a5e9d826972
bharaths1803 <bharaths14051803@gmail.com>
2024-12-26 11:35:44 +0530

    Created socket io server on top of http server

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,10 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.server.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(
@@ -35,7 +35,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
